73979971bce888460db0921242af635ff36d7e77
stefanodisanza <disanzastefano@gmail.com>
2023-01-31 17:07:06 +0100

    FEAT calcolatore prezzo finito

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-// console.log('works')
+let distance
+let age
 
-let kmDaPercorrere
-let anniPasseggero
 
-// Chiedi all’utente quanti km deve percorrere,
-kmDaPercorrere = prompt('Quanti km devi percorrere?')
-console.log('la variante km è = ', kmDaPercorrere)
+// Chiedi all’utente il suo nome,
+distance =parseFloat(prompt('Quanti km devi perccorrere?'))
+console.log('la variante km è = ', distance)
 
-// Chiedi all’utente la sua etá,
-anniPasseggero = prompt('quanti anni hai?')
-console.log('la variante anni è = ', anniPassegero)
+// poi chiedi il suo cognome,
+age = parseInt(prompt('Quanti anni hai?'))
+console.log('la variante age è = ', age)
+
+
+
+let basePrice = distance * 0.21;
+console.log('la variante prezzo base é ', basePrice)
+
+function calculatePrice(distance, age) {
+    if (age < 18){
+        return (basePrice * 0.8).toFixed(2);
+    }   else if (age >= 65) {
+        return (basePrice * 0.6).toFixed(2);
+    }   else {
+        return basePrice.toFixed(2);
+    }
+}
+
+let price = calculatePrice(distance, age);
+console.log ('la variante prezzo finale é', price)
+
+alert("Il prezzo totale del viaggio é di €" + price)
