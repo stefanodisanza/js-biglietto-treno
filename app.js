283b130cd0296faf0e784787ccb05c609230374e
stefanodisanza <disanzastefano@gmail.com>
2023-01-31 16:04:56 +0100

    START to cry, goodbye Riccardo, we will miss you!

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log('works')
+
+let kmDaPercorrere
+let anniPasseggero
+
+// Chiedi all’utente quanti km deve percorrere,
+kmDaPercorrere = prompt('Quanti km devi percorrere?')
+console.log('la variante km è = ', kmDaPercorrere)
+
+// Chiedi all’utente la sua etá,
+anniPasseggero = prompt('quanti anni hai?')
+console.log('la variante anni è = ', anniPassegero)
